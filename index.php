11f868cfe4a1b97f435c72df1850f6d11f396625
gplcom <45562816+gplcom@users.noreply.github.com>
2018-12-08 11:34:14 +0100

    Add files via upload
    RESUME

---
+++ index.php
@@ -0,0 +1,194 @@
+ 
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Developper WEB et Multi Media</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		
+	</head>
+	<body> 	
+
+	<header> 
+		<span class="centre">
+		<h1 class="name">SKILLS & COMPETENCES</h1>
+		</span> 
+	     </p>
+	</header>
+	    <span class="centre">
+	    <p style="font-weight: bold; font-size: 16px; color: grey">EXPERIENCE PROFESSIONNELLE</p>
+	    </span>
+<!-- 			
+
+<p>communication évènementielle - sale & marketing - commerce electronique</p>
+
+
+-->
+
+	<content>
+
+
+<span class="encadre exemple_margin">
+	CONCEPTION<br>
+	PROMOTION<br>
+	VENTES<br>
+</span>
+
+
+
+<div class="bloc1">
+<span><br>
+	FRANÇAIS<br>
+	ENGLISH<br>
+	PO RUCCKI<br>
+	CASTELLANO<br>
+	ITALIANO<br>
+</span>
+<div class="decalage">
+    <p>Langues de négociation</p>
+  </div>
+
+
+<div>
+			<!-- p>
+			french resume december 2018
+			<p> WEBMASTER - WEB-MARKETING PROJECT
+			</p> -->
+		</div>
+		
+
+
+</div>
+
+</content>
+<section>
+   
+ 
+
+		<div  class="img" id="picture1">
+			<p><img src="logo2.jpg" style="float:left;">Communication événementielle par le sport et l'animation sportive et de santé "triangle Organisation"<br>
+			Sales & Marketing Développement:immobilier de loisir Hotels & Resorts 5 stars affiliate RCI et Interval Internationnal<br>
+			Innovation<br>
+			Biotechnologie<br>
+
+			</p>
+			
+		</div>
+  
+  	    <div class="img" id="picture2">
+						
+		<p><img src="logo2.jpg" style="float:left;">
+			Marketing opétationnel<br>
+			Generation program<br>
+
+
+		</p>
+			
+		</div>
+	    <div  class="img" id="picture3">
+				
+	 		<p><img src="logo2.jpg" style="float:left;">
+	 		Strategie de developpement commercial<br>
+	 		Sales Network<br>
+	 		WebMarketing<br>
+	 		WebDesign<br>
+	 		WebSearch<br>
+
+
+	 		</p>
+		</div>
+		
+	
+</section>
+
+
+		<footer> 
+			
+
+			<p> &copy; gpl-Pascale </p><a href="mailto:gplcom@yahoo.es">Send an email</a>
+            <p><h1 class="mane">MARKETING</h1></p>			
+            <p style="font-style:italic;">POLYVALENCE</p>
+		
+			
+		</footer>
+	</body>
+	
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
